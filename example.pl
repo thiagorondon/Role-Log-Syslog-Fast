b4ffdc87f8976b1063f87944d57a68aec5f99e65
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2010-10-07 23:07:07 -0300

    Add README.pod and POD doc in Fast.pm
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- example.pl
+++ example.pl
@@ -1,9 +1,4 @@
 #!/usr/bin/env perl
-#
-# Aware TI, 2010, http://www.aware.com.br
-# Thiago Rondon <thiago@aware.com.br>
-#
-
 
 {
     package ExampleLog;
@@ -14,15 +9,22 @@
     use Moose;
     with 'MooseX::Log::Syslog::Fast';
 
+    sub BUILD {
+        my $self = shift;
+        $self->_hostname('/var/run/syslog');
+        $self->_name('Example');
+    }
+
     sub test {
         my $self = shift;
         $self->log('foo');
     }
+    
 }
 
 my $obj = new ExampleLog;
 
-$obj->log("foo");
+$obj->test;
 
 
 
